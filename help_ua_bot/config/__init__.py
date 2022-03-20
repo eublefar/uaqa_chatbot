00fd4bf19a3c08b93abc443cd01e01647e8d870d
eublefar <evil.unicorn1@gmail.com>
2022-03-20 03:00:14 +0100

    first commit

---
+++ help_ua_bot/config/__init__.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "{BOT_TOKEN}"
+DIRECT_LINE_SECRET = "{DIRECT_LINE_SECRET}"
+ADMIN_USER_NAMES = ["deeplearninggod"]
+QNA_MAKER_ENDPOINT = "https://ukraine-lang.cognitiveservices.azure.com/"
+QNA_SUBSCRIPTION_KEY = "{YOUR_SUBSCRIPTION_KEY}"
+KNOWLEDGEBASE_ID = "Test"
